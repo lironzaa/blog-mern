d0086e6cb6e85b4c9d74ca1557700b79850a9e66
lironzaa <lironzaa@gmail>
2020-02-20 21:43:33 +0200

    post form

--- blog-react/src/components/PostMessageForm.js
+++ blog-react/src/components/PostMessageForm.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import { TextField, withStyles, Button } from '@material-ui/core';
+import useForm from './useForm'
 
-const PostMessageForm = (props) => {
-  return (<div>from PostMessageForm</div>);
+const initialFieldValues = {
+  title: '',
+  message: ''
 }
 
-export default PostMessageForm;
+const styles = theme => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1)
+    }
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  postBtn: {
+    width: '50%'
+  }
+})
+
+const PostMessageForm = ({ classes, ...props }) => {
+
+  const {
+    values,
+    setValues,
+    handleInputChange } = useForm(initialFieldValues);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(values);
+  }
+
+  return (
+    <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+      <TextField name="title" variant="outlined" label="title"
+        fullWidth values={values.title} onChange={handleInputChange} />
+      <TextField name="message" variant="outlined" label="message"
+        fullWidth values={values.message} onChange={handleInputChange} multiline rows={4} />
+      <Button variant="contained" color="primary" size="large" type="submit" className={`${classes.postBtn}`} >submit </Button>
+    </form>
+  );
+}
+
+export default withStyles(styles)(PostMessageForm);
---
+++ blog-react/src/components/useForm.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const useForm = (initialFieldValues) => {
+  const [values, setValues] = useState(initialFieldValues);
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value
+    })
+  }
+
+  return {
+    values,
+    setValues,
+    handleInputChange
+  }
+}
+
+export default useForm;
